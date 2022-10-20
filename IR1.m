69f95f0452aa1224615fd84cea2e5a300154c61c
Øyvind Masdal Bjærum <r0921679@kuleuven.be>
2022-10-20 17:57:06 +0200

    added week 2-2

--- IR1.m
+++ IR1.m
@@ -3,12 +3,9 @@ fs = 16000;
 N = 1024;
 W = rectwin(N);
 overlap = 512;
-t = 0 : 1/fs : 5;
+impulse = [1];
 
-taps = 0 : fs*9 +127;
-impulse = t==2.49993750000000;
-impulse = double(impulse);
-
+taps = 0 : fs*4 + 255;
 [simin,nbsecs,fs] = initparams(impulse,fs);
 sim('recplay');
 
@@ -16,6 +13,7 @@ sim('recplay');
 clf;
 
 out=simout.signals.values;
+
 %%
 % impulse_response = impzest(impulse', out);
 % 
@@ -23,7 +21,8 @@ out=simout.signals.values;
 % 
 %taps = taps(end - 70000:end);
 %out = out(end - 70000:end);
+figure(1);
 subplot(2, 1, 1);
-plot(taps, out) ;
+stem(taps, out) ;
 subplot(2, 1, 2);
 pwelch( out, N, overlap, N, fs);
--- IR2.m
+++ IR2.m
@@ -1,1 +1,31 @@
-%Fill this with code plox
+fs = 16000;
+N = 1024;
+W = rectwin(N);
+overlap = 512;
+
+impulse_response_length = 5; % maybe we need to fix this
+impulse = wgn(impulse_response_length*2 + 1, 1, 0);
+%impulse(1) = 1; % Put the first stem to 1 because we want causality
+
+
+
+[simin,nbsecs,fs] = initparams(impulse,fs);
+sim('recplay');
+out=simout.signals.values;
+%%
+MAX = find(out == max(out));
+
+clipped_impulse_response = out(MAX - impulse_response_length : MAX + impulse_response_length);
+
+h = clipped_impulse_response/impulse;
+
+
+h = toeplitz(h);
+save('IRest.mat','h');
+%%
+figure(2);
+subplot(2, 1, 1);
+stem(out);
+subplot(2, 1, 2);
+pwelch( out, N, overlap, N, fs);
+
